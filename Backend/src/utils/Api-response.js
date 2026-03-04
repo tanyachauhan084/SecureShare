01dbab05bacb08dc685c6ad6e0d666bef1a913a8
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-03-04 08:55:54 +0530

    modified Api-Response class

--- Backend/src/utils/Api-response.js
+++ Backend/src/utils/Api-response.js
@@ -5,11 +5,11 @@ class ApiResponse{
        this.statuscode=statuscode,
         this.message= message
         if(statuscode>400){  //if statuscode is more than 400, it won't be successfull, it will be the error
-            success=false; //we know that when it is an error , there success will alwasy be the false in here there here there
+            this.success=false; //we know that when it is an error , there success will alwasy be the false in here there here there
         }
 
         else{
-            success=true; //if the statuscode is less than 400, then success will be true;
+            this.success=true; //if the statuscode is less than 400, then success will be true;
         }
     }
 }
