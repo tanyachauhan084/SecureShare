52b6ebef705e88f932af25e17bd99438749dcce6
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-03-04 09:47:28 +0530

    added info

--- Backend/src/routes/healthcheck.route.js
+++ Backend/src/routes/healthcheck.route.js
@@ -3,6 +3,6 @@ import healthcheck from "../controllers/healthcheck.controller.js";
 
 const Route= express.Router();
 
-Route.get("/", healthcheck);
+Route.get("/", healthcheck); // This is a route handler, router is like a middleware itself//
 
 export default Route;
