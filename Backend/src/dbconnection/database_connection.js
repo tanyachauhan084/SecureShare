26a2979c6e7ed8c2ef45de26067c82e68dc6f6a9
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-03-04 07:55:08 +0530

    DB connection

--- Backend/src/dbconnection/database_connection.js
+++ Backend/src/dbconnection/database_connection.js
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 const DB_Connection= async()=>{
 try{
    await mongoose.connect(process.env.MONGO_URI);
-   console.log("connection successfull");
+   console.log("dataBase is connected now");
    
 
 }
 
 catch(err){
-console.log("connection has failed");
+console.log("connection failed");
 process.exit(1);
 }
 }
