0fa3ead3a9e7860f59a4c028089599d36e15ee22
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-03-05 13:34:21 +0530

    renamed the file

--- Backend/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registerUser } from "../controllers/auth.controller.js";
-
-const route= express.Router();
-
-route.get("signup", registerUser);
-
-
-export {route};
