2a258dd31e3bcc3fd2d8af4be66fd58c92d25faa
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-03-05 13:31:37 +0530

    added registerUser route

---
+++ Backend/src/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { registerUser } from "../controllers/auth.controller.js";
+
+const route= express.Router();
+
+route.get("/signup", registerUser);
+
+
+export default route;
