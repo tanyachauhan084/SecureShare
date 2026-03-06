5d0ff092a347e5de13302314f65d6215130b5b02
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-03-06 08:00:38 +0530

    modified error Middleware

--- Backend/src/middlewares/error_middleware.js
+++ Backend/src/middlewares/error_middleware.js
@@ -5,9 +5,10 @@ const errorMiddleware= (err,req,res,next)=>{
     if(err instanceof ApiError){
         return res.status(err.statuscode).json(
            {
-                sucess:false,
+                data:err.data,
                 statuscode:err.statuscode,
-                message: err.message
+                message: err.message,
+                sucess:false
 
            }
             
@@ -16,11 +17,14 @@ const errorMiddleware= (err,req,res,next)=>{
 
     // this is an error middle, so you not have to return an error,onlyy you can either use noting just return res or use the Apiresponse..//
     //obv error middelware does not return another error, it resolves error and it already has an error which an either be thei nsatance of Apierror or a random diff error//
-    return res.status(400).json(
+   
+   
+    return res.status(500).json(
       {
            data:"it is an error",
            statuscode:500,
-            message: "something went wrong"
+            message: "something went wrong",
+            success:false
 
       }
         
