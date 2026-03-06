ba1f6a8421e627892b1949a90a8be8a4868e3a5f
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-03-06 08:40:43 +0530

    resolved a bug in the password Verification service

--- Backend/src/services/passwordVerification.services.js
+++ Backend/src/services/passwordVerification.services.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 const passwordVerification= async(password, hashedPassword)=>{
 
      //it will return a booleean, true if the password is verified, false, if the password is not verified..//
-      await bcrypt.compare(password, hashedPassword); //return a boolean value;
+     return await bcrypt.compare(password, hashedPassword); //return a boolean value;
 }
 
 export default passwordVerification;
