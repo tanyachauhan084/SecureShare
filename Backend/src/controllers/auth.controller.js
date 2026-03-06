7095e8cf4a98872abbcf45212850dcf8b684c8bd
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-03-06 08:42:23 +0530

    added loginUser logic(controller)

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import passwordVerification from "../services/passwordVerification.services.js";
 import { ApiError } from "../utils/Api-error.js";
 import ApiResponse from "../utils/Api-response.js";
 import async_handler from "../utils/async_handler.js";
@@ -57,4 +58,58 @@ const registerUser= async_handler(async(req, res)=>{
 
 })
 
-export  {registerUser};
+//login controller
+
+const loginUser= async_handler(async(req,res)=>{
+
+//login->email, password
+
+ const {email, password}= req.body;
+
+ //check if there is an existing user with this emailid//
+
+ const user= await User.findOne({email});
+
+
+ //the user does not exist with the given emailid
+ if(!user){
+    //an instance of Apierror
+    throw new ApiError(
+            "An error occured",
+            404, //NOT FOUND
+            "No user exists with this emailId, try with a different emailId"
+
+    )
+}
+
+
+//if the user exist with that emailid, then validate and verify the password wiht hashed password stored in the dtaabase;;//
+
+const isMatch= passwordVerification(password, user.password);
+
+//if password is not verified//
+if(!isMatch){
+
+    throw new ApiError(
+
+        "An error occured",
+        401, //UNAUTHORIZED REQUEST
+        "Incorrect password, try again"
+    )
+}
+
+//if password is a match, if ture, so now we know that the user exists in the db, it is an already register user in  database//
+
+const loggedInUser= await User.findById(user._id).select("-password");
+
+return res.status(201).json(
+    new ApiResponse(
+        loggedInUser,
+        201,
+        "User successfully logged In"
+    )
+)
+
+})
+
+export  {registerUser, loginUser};
